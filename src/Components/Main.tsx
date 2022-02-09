9d4c80bdaf0a4a52530b2de27ca0a395014c8da8
Nur Egal <nuregal87@gmail.com>
2022-02-09 10:50:11 +0100

    file update

--- src/Components/Main.tsx
+++ src/Components/Main.tsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from "react";
 import Event from "./Event";
 import events, { handleLocalStorage } from "../data/eventsData";
-import { meetup } from "../models/meetup";
 interface Props {
   updateCount(): void;
 }
--- src/Test/Main.test.tsx
+++ src/Test/Main.test.tsx
@@ -2,8 +2,12 @@ import React from 'react';
 import { render, screen } from "@testing-library/react";
 import Main from '../Components/Main';
 
+interface Props {
+  updateCount(): void;
+}
+
 describe('Main Component', () => {
     it('renders correctly',() => {
-        render(<Main />)
+        render(<Main updateCount = {() =>{}}/>)
     })
 })
